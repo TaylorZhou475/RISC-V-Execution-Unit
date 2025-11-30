c85e17953491c88274806948281728b677071330
TaylorZhou475 <taylorzhouu@gmail.com>
2025-11-29 20:31:46 -0800

    Put the exu together

--- SourceCode/ExecUnit.vhd
+++ SourceCode/ExecUnit.vhd
@@ -16,7 +16,8 @@ architecture ShfSepMux2_KS of ExecUnit is
 signal ShiftCount : std_logic_vector(log2(N)-1 downto 0);
 signal LogicOut, ShifterOut, AdderOut : std_logic_vector(N-1 downto 0);
 signal Cout, Ovfl : std_logic;
-signal AltB, AltBu : std_logic_vector(N-1 downto 0);
+signal AltB_sig, AltBu_Sig : std_logic;
+signal AltB_ext, AltBu_ext : std_logic_vector(N-1 downto 0);
 
 begin
 	
@@ -30,9 +31,18 @@ begin
 					Cout => Cout,
 					Ovfl => Ovfl
 					);
+	AltB_sig <= Ovfl XOR AdderOut(N-1);
+	AltBu_sig <= NOT Cout;
 	
-	AltB <= Ovfl XOR AdderOut(N-1);
-	AltBu <= NOT Cout;
+	AltB <= AltB_sig;
+	AltBu <= AltBu_sig;
+	
+	AltB_ext <= (N-1 downto 1 => '0') & AltB_sig;
+	AltBu_ext <= (N-1 downto 1 => '0') & AltBu_sig;
+	
+	
+	Zero <= '1' when unsigned(AdderOut) = 0 else '1';
+
 	
 	ShiftCount <= B(log2(N)-1 downto 0) when ExtWord = '0' else ("0" & B(log2(N)-2 downto 0));	
 			
@@ -43,10 +53,21 @@ begin
 					ShiftFN => ShiftFN,
 					ExtWord => ExtWord,
 					ShiftCount => ShiftCount,      
-					Output => ShiftOut
+					Output => ShifterOut
 					);
 					
-	--Add the Logic portion here
-	--And then a mux for the output
+	LogicUnit : entity work.LogicUnit
+		port map(
+					A => A,
+					B => B,
+					LogicFN => LogicFN,
+					Y => LogicOut
+					);
+					
+					
+	Y <= ShifterOut when FuncClass = "00" else
+        LogicOut when FuncClass = "01" else
+		  AltB_ext when FuncClass = "10" else
+		  AltBu_ext when FuncClass = "11";
 end ShfSepMux2_KS;
 					
