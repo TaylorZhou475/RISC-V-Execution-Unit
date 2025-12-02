deea9947f79ed0a1fc458efd5c33494cdc20da32
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2025-12-01 19:11:50 -0800

    Fixed carry skip
    Carry Skip had compilation issues in quartus related to keyword naming and unary operation.
    
    Fixes include a psuedo func to act as unary operator, renaming and fixing internal logic muxes

--- SourceCode/Adder Implementations/CarrySkip.vhd
+++ SourceCode/Adder Implementations/CarrySkip.vhd
@@ -34,67 +34,84 @@ architecture rtl of CarrySkip is
 		  
     end component;
 
-    -- block size
-    constant BLOCK_SIZE : integer := 8;
-    constant N_BLOCKS : integer := N/BLOCK_SIZE;
+    -- blk_c size
+    constant blk_c_SIZE : integer := 8;
+    constant N_blk_cS : integer := N/blk_c_SIZE;
 
-    -- block: carry signals between blocks (can be skipped)
-    -- ripple: carry signals for the ripple chain within each block
-    signal block  : std_logic_vector(N_BLOCKS downto 0);
+    -- blk_c: carry signals between blk_cs (can be skipped)
+    -- ripple: carry signals for the ripple chain within each blk_c
+    signal blk_c  : std_logic_vector(N_blk_cS downto 0);
     signal ripple : std_logic_vector(N downto 0);
 
-    -- group: block-level propagate signal
-    signal group  : std_logic_vector(N_BLOCKS-1 downto 0);
+    -- grp_c: blk_c-level propagate signal
+    signal grp_c  : std_logic_vector(N_blk_cS-1 downto 0);
 
 begin
 
-    block(0) <= Cin;
-    ripple <= (0 => Cin, others => '0');
+    blk_c(0) <= Cin;
+    --ripple <= (0 => Cin, others => '0');
 
-    gen_blocks: for i in 0 to N_BLOCKS-1 generate
+    gen_blk_cs: for i in 0 to N_blk_cS-1 generate
         
-        -- This is '1' if all P bits in the block are '1'
-        signal bits : std_logic_vector(BLOCK_SIZE-1 downto 0);
+        -- This is '1' if all P bits in the blk_c are '1'
+        signal bits : std_logic_vector(blk_c_SIZE-1 downto 0);
 		  
     begin
 	 
-        gen_bits: for j in 0 to BLOCK_SIZE-1 generate
-            bits(j) <= A(i*BLOCK_SIZE + j) xor B(i*BLOCK_SIZE + j);
+        gen_bits: for j in 0 to blk_c_SIZE-1 generate
+            bits(j) <= A(i*blk_c_SIZE + j) xor B(i*blk_c_SIZE + j);
         end generate;
 		  
-        group(i) <= '&' & bits;
+        --grp_c(i) <= '&' & bits; --this was being buggy, preventing compiling
+		  process(bits)
+            variable tmp : std_logic;
+        begin
+            tmp := '1';
+            for k in bits'range loop
+                tmp := tmp and bits(k);
+            end loop;
+            grp_c(i) <= tmp;
+        end process;
 
-        -- Generate the ripple-carry chain within the block
-        gen_rippleadder: for j in 0 to BLOCK_SIZE-1 generate
-            constant index : integer := i*BLOCK_SIZE + j;
+        -- Generate the ripple-carry chain within the blk_c
+        gen_rippleadder: for j in 0 to blk_c_SIZE-1 generate
+            constant index : integer := i*blk_c_SIZE + j;
+				signal fa_cin : std_logic;
 				
         begin
+				--need muxes here
+				--if at start of block (j=0), pull carry from skip chain
+				--otherwise, pull carry from previous adder (ripple)
+				
+				fa_cin <= blk_c(i) when (j=0) else ripple(index);
 		  
             FA_inst: FullAdder
 				
                 port map(
                     x    => A(index),
                     y    => B(index),
-                    cin  => ripple(index),
+                    cin  => fa_cin,
                     sum  => S(index),
                     cout => ripple(index+1)
                 );
         end generate;
         
         -- Carry-Skip MUX Logic
-        -- The carry for the next block is either the result of the internal
-        -- ripple chain OR the skipped incoming carry if the block propagates.
-        block(i+1) <= ripple((i+1)*BLOCK_SIZE) or (block(i) and group(i));
+        -- The carry for the next blk_c is either the result of the internal
+        -- ripple chain OR the skipped incoming carry if the blk_c propagates.
+        blk_c(i+1) <= ripple((i+1)*blk_c_SIZE) or (blk_c(i) and grp_c(i));
         
-        -- The start of the next block's ripple chain is the result of the skip logic
-        gen_cin: if i < N_BLOCKS-1 generate
-            ripple((i+1)*BLOCK_SIZE) <= block(i+1);
-        end generate;
+        -- The start of the next blk_c's ripple chain is the result of the skip logic
+		  
+		  --i dont think we need this block anymore, ripple should only hold internal slow chain
+--        gen_cin: if i < N_blk_cS-1 generate
+--            ripple((i+1)*blk_c_SIZE) <= blk_c(i+1);
+--        end generate;
 
-    end generate gen_blocks;
+    end generate gen_blk_cs;
 
     -- Final outputs
-    Cout <= block(N_BLOCKS);
-    Ovfl <= block(N_BLOCKS) xor ripple(N-1);
+    Cout <= blk_c(N_blk_cS);
+    Ovfl <= blk_c(N_blk_cS) xor ripple(N-1);
 
 end architecture rtl;
